6c117071761391a634ea19e32fe4ce8ff88620d7
ggimenez <ggimenez@academic.rrc.ca>
2023-06-03 17:04:37 -0500

    Add products seed data and Products controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+# Generate 676 products
+676.times do
+  Product.create(
+    title: Faker::Commerce.product_name,
+    price: Faker::Commerce.price(range: 0..100.0),
+    stock_quantity: Faker::Number.between(from: 0, to: 100)
+  )
+end
+
